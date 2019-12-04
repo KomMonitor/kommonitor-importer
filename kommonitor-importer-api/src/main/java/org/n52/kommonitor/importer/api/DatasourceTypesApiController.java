c550c02df07d62c3a8de8bfac4ed722beb5aa293
Sebastian Drost <s.drost@52north.org>
2019-12-04 17:23:37 +0100

    Implement API for retreiving supported datasource types

--- kommonitor-importer-api/src/main/java/org/n52/kommonitor/importer/api/DatasourceTypesApiController.java
+++ kommonitor-importer-api/src/main/java/org/n52/kommonitor/importer/api/DatasourceTypesApiController.java
@@ -1,32 +1,39 @@
 package org.n52.kommonitor.importer.api;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.annotations.ApiParam;
+import org.n52.kommonitor.importer.api.encoder.DataSourceRetrieverEncoder;
+import org.n52.kommonitor.importer.api.utils.ErrorFactory;
+import org.n52.kommonitor.importer.io.datasource.AbstractDataSourceRetriever;
+import org.n52.kommonitor.importer.io.datasource.DataSourceRetrieverRepository;
 import org.n52.kommonitor.importer.models.DataSourceType;
 import org.n52.kommonitor.importer.models.Error;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.*;
-import javax.validation.Valid;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-12-04T14:17:33.692+01:00")
 
 @Controller
 public class DatasourceTypesApiController implements DatasourceTypesApi {
 
     private static final Logger log = LoggerFactory.getLogger(DatasourceTypesApiController.class);
+
+    @Autowired
+    private DataSourceRetrieverRepository retrieverRepository;
+
+    @Autowired
+    private DataSourceRetrieverEncoder encoder;
 
     private final ObjectMapper objectMapper;
 
@@ -35,35 +42,25 @@ public class DatasourceTypesApiController implements DatasourceTypesApi {
     @org.springframework.beans.factory.annotation.Autowired
     public DatasourceTypesApiController(ObjectMapper objectMapper, HttpServletRequest request) {
         this.objectMapper = objectMapper;
+        this.objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         this.request = request;
     }
 
-    public ResponseEntity<DataSourceType> getSupportedDataSourceTypeByType(@ApiParam(value = "unique type of the datasource",required=true) @PathVariable("type") String type) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<DataSourceType>(objectMapper.readValue("{  \"type\" : \"DB\",  \"parameters\" : [ {    \"name\" : \"name\",    \"description\" : \"description\",    \"type\" : \"string\"  }, {    \"name\" : \"name\",    \"description\" : \"description\",    \"type\" : \"string\"  } ]}", DataSourceType.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<DataSourceType>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
 
-        return new ResponseEntity<DataSourceType>(HttpStatus.NOT_IMPLEMENTED);
+    public ResponseEntity<DataSourceType> getSupportedDataSourceTypeByType(@ApiParam(value = "unique type of the datasource", required = true) @PathVariable("type") String type) {
+        Optional<AbstractDataSourceRetriever> retrieverOpt = retrieverRepository.getDatasourceRetreiver(type);
+        if (!retrieverOpt.isPresent()) {
+            Error error = ErrorFactory.getError(HttpStatus.NOT_FOUND.value(), "No support for the specified datasource type: " + type);
+            ResponseEntity entity = ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+            return entity;
+        }
+        return new ResponseEntity<DataSourceType>(encoder.encode(retrieverOpt.get()), HttpStatus.OK);
     }
 
     public ResponseEntity<List<DataSourceType>> getSupportedDataSourceTypes() {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<List<DataSourceType>>(objectMapper.readValue("[ {  \"type\" : \"DB\",  \"parameters\" : [ {    \"name\" : \"name\",    \"description\" : \"description\",    \"type\" : \"string\"  }, {    \"name\" : \"name\",    \"description\" : \"description\",    \"type\" : \"string\"  } ]}, {  \"type\" : \"DB\",  \"parameters\" : [ {    \"name\" : \"name\",    \"description\" : \"description\",    \"type\" : \"string\"  }, {    \"name\" : \"name\",    \"description\" : \"description\",    \"type\" : \"string\"  } ]} ]", List.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<List<DataSourceType>>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<List<DataSourceType>>(HttpStatus.NOT_IMPLEMENTED);
+        return new ResponseEntity<List<DataSourceType>>(retrieverRepository.getAll().stream()
+                .map(r -> encoder.simpleEncode(r))
+                .collect(Collectors.toList()), HttpStatus.OK);
     }
 
 }
---
+++ kommonitor-importer-api/src/main/java/org/n52/kommonitor/importer/api/encoder/DataSourceRetrieverEncoder.java
@@ -0,0 +1,70 @@
+package org.n52.kommonitor.importer.api.encoder;
+
+import org.n52.kommonitor.importer.converter.AbstractConverter;
+import org.n52.kommonitor.importer.converter.ConverterParameters;
+import org.n52.kommonitor.importer.io.datasource.AbstractDataSourceRetriever;
+import org.n52.kommonitor.importer.io.datasource.DataSourceParameter;
+import org.n52.kommonitor.importer.io.datasource.DataSourceRetriever;
+import org.n52.kommonitor.importer.models.ConverterParameterType;
+import org.n52.kommonitor.importer.models.ConverterType;
+import org.n52.kommonitor.importer.models.DataSourceType;
+import org.n52.kommonitor.importer.models.ParameterType;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * @author @author <a href="mailto:s.drost@52north.org">Sebastian Drost</a>
+ */
+@Component
+public class DataSourceRetrieverEncoder {
+
+    /**
+     * Encodes an {@link AbstractDataSourceRetriever} into a corresponding type for API responses
+     *
+     * @param retriever the {@link AbstractDataSourceRetriever} to convert
+     * @return the encoded retriever as {@link DataSourceType}
+     */
+    public DataSourceType encode(AbstractDataSourceRetriever retriever) {
+        DataSourceType dataSourceType = simpleEncode(retriever);
+
+        if (retriever.getDataSourceParameters() != null && !retriever.getDataSourceParameters().isEmpty()) {
+            Set<DataSourceParameter> parameters = retriever.getDataSourceParameters();
+            dataSourceType.setParameters(parameters.stream()
+                    .map(this::encodeParameters)
+                    .collect(Collectors.toList()));
+        }
+
+        return dataSourceType;
+    }
+
+    /**
+     * Encodes an {@link AbstractDataSourceRetriever} into a corresponding type for API responses
+     * without parameters.
+     *
+     * @param retriever the {@link AbstractDataSourceRetriever} to convert
+     * @return the encoded retriever as {@link DataSourceType}
+     */
+    public DataSourceType simpleEncode(AbstractDataSourceRetriever retriever) {
+        DataSourceType dataSourceType = new DataSourceType();
+        dataSourceType.setType(DataSourceType.TypeEnum.valueOf(retriever.getType()));
+
+        return dataSourceType;
+    }
+
+    /**
+     * Encodes a {@link DataSourceParameter} into a corresponding type for API responses
+     *
+     * @param parameter the {@link DataSourceParameter} to convert
+     * @return the encoded parameters as {@link ParameterType}
+     */
+    public ParameterType encodeParameters(DataSourceParameter parameter) {
+        ParameterType paramType = new ParameterType();
+        paramType.setName(parameter.getName());
+        paramType.setDescription(parameter.getDescription());
+        paramType.setType(ParameterType.TypeEnum.valueOf(parameter.getType().name()));
+        return paramType;
+    }
+}
