ae71dff54c36b95b261b532241555e448f3c498a
Sebastian Drost <s.drost@52north.org>
2019-12-12 10:50:00 +0100

    Add ConverterException

--- kommonitor-importer-core/src/main/java/org/n52/kommonitor/importer/converter/Converter.java
+++ kommonitor-importer-core/src/main/java/org/n52/kommonitor/importer/converter/Converter.java
@@ -3,8 +3,12 @@ package org.n52.kommonitor.importer.converter;
 import org.n52.kommonitor.importer.entities.Dataset;
 import org.n52.kommonitor.importer.entities.Indicator;
 import org.n52.kommonitor.importer.entities.SpatialResource;
+import org.n52.kommonitor.importer.exceptions.ConverterException;
 import org.n52.kommonitor.importer.models.ConverterDefinitionType;
-import org.n52.kommonitor.importer.models.DataSourceType;
+import org.n52.kommonitor.importer.models.IndicatorPropertyMappingType;
+import org.n52.kommonitor.importer.models.SpatialResourcePropertyMappingType;
+
+import java.util.List;
 
 /**
  * An interface for converting spatial resources and indicators from a certain dataset format into
@@ -17,21 +21,21 @@ public interface Converter {
     String getName();
 
     /**
-     * Converts a {@link Dataset} into a {@link SpatialResource}  Faccording to a given {@link ConverterDefinitionType}
+     * Converts a {@link Dataset} into {@link List<SpatialResource>}  according to a given {@link ConverterDefinitionType}
      *
      * @param importerDefinition the {@link ConverterDefinitionType} used to decode the dataset
      * @param dataset            the {@link Dataset} that contains the data to convert
-     * @return the converted {@link SpatialResource}
+     * @return the converted {@link List<SpatialResource>}}
      */
-    SpatialResource convertSpatialResource(ConverterDefinitionType importerDefinition, Dataset dataset);
+    List<SpatialResource> convertSpatialResources(ConverterDefinitionType importerDefinition, Dataset dataset, SpatialResourcePropertyMappingType propertyMapping) throws ConverterException;
 
     /**
-     * Decodes a dataset into a {@link Indicator} according to a given {@link ConverterDefinitionType}
+     * Decodes a dataset into {@link List<Indicator>} according to a given {@link ConverterDefinitionType}
      *
      * @param importerDefinition the {@link ConverterDefinitionType} used to decode the dataset
      * @param dataset            the {@link Dataset} that contains the data to convert
-     * @return the converted {@link Indicator}
+     * @return the converted {@link List<Indicator>}
      */
-    Indicator convertIndicator(ConverterDefinitionType importerDefinition, Dataset dataset);
+    List<Indicator> convertIndicators(ConverterDefinitionType importerDefinition, Dataset dataset, IndicatorPropertyMappingType propertyMapping) throws ConverterException;
 
 }
---
+++ kommonitor-importer-core/src/main/java/org/n52/kommonitor/importer/exceptions/ConverterException.java
@@ -0,0 +1,21 @@
+package org.n52.kommonitor.importer.exceptions;
+
+/**
+ * Indicates that an error has occurred during dataset converting
+ *
+ * @author @author <a href="mailto:s.drost@52north.org">Sebastian Drost</a>
+ */
+public class ConverterException extends Exception {
+
+    public ConverterException(String message) {
+        super(message);
+    }
+
+    public ConverterException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ConverterException(Throwable cause) {
+        super(cause);
+    }
+}
