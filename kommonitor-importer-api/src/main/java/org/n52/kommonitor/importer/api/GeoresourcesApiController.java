0635d965e0d25c48dde4a463779bfc4697b29bfc
Sebastian Drost <s.drost@52north.org>
2019-12-12 11:28:12 +0100

    Add handler for Georesource imports

--- kommonitor-importer-api/src/main/java/org/n52/kommonitor/importer/api/GeoresourcesApiController.java
+++ kommonitor-importer-api/src/main/java/org/n52/kommonitor/importer/api/GeoresourcesApiController.java
@@ -1,32 +1,51 @@
 package org.n52.kommonitor.importer.api;
 
+import org.n52.kommonitor.importer.api.handler.GeoresourceImportHandler;
+import org.n52.kommonitor.importer.api.utils.ErrorFactory;
+import org.n52.kommonitor.importer.converter.AbstractConverter;
+import org.n52.kommonitor.importer.exceptions.ConverterException;
+import org.n52.kommonitor.importer.converter.ConverterRepository;
+import org.n52.kommonitor.importer.entities.Dataset;
+import org.n52.kommonitor.importer.entities.SpatialResource;
+import org.n52.kommonitor.importer.exceptions.ImportParameterException;
+import org.n52.kommonitor.importer.io.datasource.AbstractDataSourceRetriever;
+import org.n52.kommonitor.importer.exceptions.DataSourceRetrieverException;
+import org.n52.kommonitor.importer.io.datasource.DataSourceRetrieverRepository;
 import org.n52.kommonitor.importer.models.Error;
 import org.n52.kommonitor.importer.models.ImportGeoresourcePOSTInputType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-import javax.validation.constraints.*;
 import javax.validation.Valid;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.util.List;
+import java.util.*;
+
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-12-02T16:59:46.021+01:00")
 
 @Controller
 public class GeoresourcesApiController implements GeoresourcesApi {
 
     private static final Logger log = LoggerFactory.getLogger(GeoresourcesApiController.class);
+
+    @Autowired
+    private ConverterRepository converterRepository;
+
+    @Autowired
+    private DataSourceRetrieverRepository retrieverRepository;
+
+    @Autowired
+    private GeoresourceImportHandler importHandler;
 
     private final ObjectMapper objectMapper;
 
@@ -38,18 +57,9 @@ public class GeoresourcesApiController implements GeoresourcesApi {
         this.request = request;
     }
 
-    public ResponseEntity<List<String>> importGeoresource(@ApiParam(value = "feature data" ,required=true )  @Valid @RequestBody ImportGeoresourcePOSTInputType featureData) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<List<String>>(objectMapper.readValue("[ \"\", \"\" ]", List.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<List<String>>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<List<String>>(HttpStatus.NOT_IMPLEMENTED);
+    public ResponseEntity<List<String>> importGeoresource(@ApiParam(value = "feature data", required = true) @Valid @RequestBody ImportGeoresourcePOSTInputType featureData) {
+        return importHandler.handleImportRequest(featureData, featureData.getDataSource(), featureData.getConverter());
     }
+
 
 }
---
+++ kommonitor-importer-api/src/main/java/org/n52/kommonitor/importer/api/handler/GeoresourceImportHandler.java
@@ -0,0 +1,48 @@
+package org.n52.kommonitor.importer.api.handler;
+
+import org.n52.kommonitor.datamanagement.api.client.GeoresourcesApi;
+import org.n52.kommonitor.datamanagement.api.models.GeoresourcePOSTInputType;
+import org.n52.kommonitor.importer.api.encoder.GeoresourceEncoder;
+import org.n52.kommonitor.importer.converter.AbstractConverter;
+import org.n52.kommonitor.importer.entities.Dataset;
+import org.n52.kommonitor.importer.entities.SpatialResource;
+import org.n52.kommonitor.importer.exceptions.ConverterException;
+
+import org.n52.kommonitor.importer.models.ConverterDefinitionType;
+import org.n52.kommonitor.importer.models.ImportGeoresourcePOSTInputType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+
+import java.util.List;
+
+/**
+ * Handles a georesource import request by converting a {@link Dataset} into a {@link List<SpatialResource>} and
+ * mapping
+ *
+ * @author <a href="mailto:s.drost@52north.org">Sebastian Drost</a>
+ */
+@Component
+public class GeoresourceImportHandler extends AbstractImportHandler<ImportGeoresourcePOSTInputType> {
+    private static final String LOCATION_HEADER_KEY = "location";
+
+    @Autowired
+    private GeoresourceEncoder encoder;
+
+    @Autowired
+    private GeoresourcesApi apiClient;
+
+    @Override
+    public ResponseEntity importResource(ImportGeoresourcePOSTInputType importResourceType, AbstractConverter converter, ConverterDefinitionType converterDefinition, Dataset dataset) throws ConverterException, RestClientException {
+        List<SpatialResource> spatialResources = converter.convertSpatialResources(converterDefinition, dataset, importResourceType.getPropertyMapping());
+        GeoresourcePOSTInputType georesourcePostInput = encoder.encode(importResourceType, spatialResources);
+
+        ResponseEntity<Void> response = apiClient.addGeoresourceAsBodyWithHttpInfo(georesourcePostInput);
+        List<String> locations = response.getHeaders().get(LOCATION_HEADER_KEY);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(locations);
+    }
+}
