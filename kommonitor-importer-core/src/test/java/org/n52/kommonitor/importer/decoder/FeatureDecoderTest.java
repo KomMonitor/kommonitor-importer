60a18c06f1a3c622c72b24558bea41c74fc9d387
Sebastian Drost <s.drost@52north.org>
2019-12-17 17:43:46 +0100

    Add feature decoding tests

--- kommonitor-importer-core/src/test/java/org/n52/kommonitor/importer/decoder/FeatureDecoderTest.java
+++ kommonitor-importer-core/src/test/java/org/n52/kommonitor/importer/decoder/FeatureDecoderTest.java
@@ -1,38 +1,87 @@
 package org.n52.kommonitor.importer.decoder;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
+import org.n52.kommonitor.importer.exceptions.DecodingException;
+import org.opengis.feature.simple.SimpleFeature;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 /**
  * @author <a href="mailto:s.drost@52north.org">Sebastian Drost</a>
  */
 class FeatureDecoderTest {
 
-    @BeforeEach
-    void setUp() {
+    private static FeatureDecoder decoder;
+
+    @BeforeAll
+    static void setUp() {
+        decoder = new FeatureDecoder();
+    }
+    
+    @Test
+    void testGetStringPropertyValue() throws DecodingException {
+        SimpleFeature feature = Mockito.mock(SimpleFeature.class);
+        String attName = "testAttribute";
+        String attValue = "testAttributeValue";
+        Mockito.when(feature.getAttribute(attName)).thenReturn(attValue);
+
+        Assertions.assertEquals(attValue, decoder.getPropertyValueAsString(feature, attName));
     }
 
     @Test
-    void decodeFeature() {
+    @DisplayName("Test get String property should throw an exception for an invalid type")
+    void testGetStringPropertyValueThrowsExceptionForInvalidType() {
+        SimpleFeature feature = Mockito.mock(SimpleFeature.class);
+        String attName = "testAttribute";
+        Mockito.when(feature.getAttribute(attName)).thenReturn(Integer.valueOf(1));
+
+        Assertions.assertThrows(DecodingException.class, () -> decoder.getPropertyValueAsString(feature, attName));
     }
 
     @Test
-    void decodeFeatureCollection() {
+    @DisplayName("Test get Date property should throw an exception for an invalid type")
+    void testGetDatePropertyValue() throws DecodingException {
+        SimpleFeature feature = Mockito.mock(SimpleFeature.class);
+        String attName = "testAttribute";
+        Mockito.when(feature.getAttribute(attName)).thenReturn(Integer.valueOf(1));
+
+        Assertions.assertThrows(DecodingException.class, () -> decoder.getPropertyValueAsDate(feature, attName));
     }
 
     @Test
-    @DisplayName("An awesome test")
-    void getGeometry() {
+    void testGetDatePropertyValueThrowsExceptionForUnvalidType() throws DecodingException {
+        SimpleFeature feature = Mockito.mock(SimpleFeature.class);
+        String attName = "testAttribute";
+        Date date = new Date();
+        Mockito.when(feature.getAttribute(attName)).thenReturn(date);
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        Assertions.assertEquals(df.format(date), dtf.format(decoder.getPropertyValueAsDate(feature, attName)));
     }
 
     @Test
-    void getStringPropertyValue() {
+    @DisplayName("Test get property value for existing property")
+    void testGetPropertyValue() throws DecodingException {
+        SimpleFeature feature = Mockito.mock(SimpleFeature.class);
+        String attName = "testAttribute";
+        String attValue = "testAttributeValue";
+        Mockito.when(feature.getAttribute(attName)).thenReturn(attValue);
+
+        Assertions.assertEquals(attValue, decoder.getPropertyValue(feature, attName));
     }
 
     @Test
-    void getDatePropertyValue() {
+    @DisplayName("Test get property value should throw exception for missing property")
+    void testGetPropertyValueShouldThrowExceptionForMissingProperty() {
+        SimpleFeature feature = Mockito.mock(SimpleFeature.class);
+        String attName = "testAttribute";
+
+        Assertions.assertThrows(DecodingException.class, () -> decoder.getPropertyValue(feature, attName));
     }
 }
