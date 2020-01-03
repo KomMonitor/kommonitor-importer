8bd43d30250fb2742e5d4df021d04b96e5aeeec8
Sebastian Drost <s.drost@52north.org>
2020-01-03 13:26:22 +0100

    Add tests for SpatialResource converting

--- kommonitor-importer-core/src/test/java/org/n52/kommonitor/importer/converter/WFSv1ConverterTest.java
+++ kommonitor-importer-core/src/test/java/org/n52/kommonitor/importer/converter/WFSv1ConverterTest.java
@@ -1,18 +1,12 @@
 package org.n52.kommonitor.importer.converter;
 
-//import net.opengis.wfs.x20.FeatureCollectionDocument;
-//import net.opengis.wfs.x20.FeatureCollectionType;
-
-import org.apache.commons.lang3.builder.ToStringExclude;
 import org.geotools.GML;
-import org.geotools.geometry.jts.WKTReader2;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.locationtech.jts.geom.Geometry;
-import org.locationtech.jts.io.ParseException;
-import org.locationtech.jts.io.WKTReader;
+import org.mockito.Mockito;
 import org.n52.kommonitor.importer.entities.Dataset;
 import org.n52.kommonitor.importer.entities.SpatialResource;
 import org.n52.kommonitor.importer.exceptions.ConverterException;
@@ -20,7 +14,6 @@ import org.n52.kommonitor.importer.exceptions.ImportParameterException;
 import org.n52.kommonitor.importer.models.ConverterDefinitionType;
 import org.n52.kommonitor.importer.models.SpatialResourcePropertyMappingType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -34,11 +27,30 @@ import java.util.List;
 @ContextConfiguration(classes = {WFSv1Converter.class})
 public class WFSv1ConverterTest {
 
-    private String WFS_SCHEMA = "http://schemas.opengis.net/wfs/1.0.0/wfs.xsd";
+    private final static String WFS_SCHEMA = "http://schemas.opengis.net/wfs/1.0.0/wfs.xsd";
+    private final static String MIME_TYPE = "application/xml";
+    private final static String ENCODING = "UTF-8";
+
+    private static ConverterDefinitionType convDef;
+    private static SpatialResourcePropertyMappingType propertyMapping;
 
     @Autowired
     private WFSv1Converter converter;
-    
+
+    @BeforeAll
+    static void init() {
+        convDef = new ConverterDefinitionType();
+        convDef.setMimeType(MIME_TYPE);
+        convDef.setSchema(WFS_SCHEMA);
+        convDef.setEncoding(ENCODING);
+
+        propertyMapping = new SpatialResourcePropertyMappingType();
+        propertyMapping.setIdentifierProperty("Baublock_ID");
+        propertyMapping.setNameProperty("Baublock_ID");
+        propertyMapping.setValidStartDateProperty("EreignisintervallStart");
+        propertyMapping.setValidEndDateProperty("EreignisintervallEnde");
+    }
+
     @Test
     @DisplayName("Test get GML parser for a certain schema")
     void testGetGmlParserForSchema() {
@@ -51,6 +63,35 @@ public class WFSv1ConverterTest {
     @DisplayName("Test get GML parser throws ImportParameterException for unsupported schema")
     void testGetGmlThrowsImportParameterExceptionForUnsupportedSchema() {
         Assertions.assertThrows(ImportParameterException.class, () -> converter.getGmlParserForSchema("http://no/valid/gml/schema"));
+    }
+
+    @Test
+    @DisplayName("Test convert SpatialResources for WFS 1.0.0 datasource")
+    void testConvertSpatialResourcesForWfs100Datasource() throws ConverterException {
+        InputStream input = getClass().getResourceAsStream("/getWfs100FeatureResponseTest.xml");
+        Dataset<InputStream> dataset = new Dataset<>(input);
+
+        List<SpatialResource> spatialResources = converter.convertSpatialResources(convDef, dataset, propertyMapping);
+
+        Assertions.assertEquals(3, spatialResources.size());
+    }
+
+    @Test
+    @DisplayName("Test convert SpatialResources for WFS datasource throws ConverterException for unsupported dataset type")
+    void testConvertSpatialResourcesForWfsDatasourceThrowsConverterExceptionForUnsupportedDatasetType() {
+        ConverterDefinitionType convDef = Mockito.mock(ConverterDefinitionType.class);
+        SpatialResourcePropertyMappingType propertyMapping = Mockito.mock(SpatialResourcePropertyMappingType.class);
+        Dataset<Double> dataset = new Dataset<>(123.123);
+
+        Assertions.assertThrows(ConverterException.class, () -> converter.convertSpatialResources(convDef, dataset, propertyMapping));
+    }
+
+    @Test
+    @DisplayName("Test convert SpatialResources for WFS datasource throws ConverterException for non parsable dataset")
+    void testConvertSpatialResourcesForWfsDatasourceThrowsConverterExceptionForNotParsableDataset() {
+        Dataset<String> dataset = new Dataset<>("nonParsableFeatureCollection");
+
+        Assertions.assertThrows(ConverterException.class, () -> converter.convertSpatialResources(convDef, dataset, propertyMapping));
     }
 
 }
