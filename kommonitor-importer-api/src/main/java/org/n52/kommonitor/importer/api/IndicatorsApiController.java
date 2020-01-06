7728fb39c1d62766287d77cb0029adb0732ec9b9
Sebastian Drost <s.drost@52north.org>
2020-01-06 17:22:48 +0100

    Implement Indicator import endpoint

--- kommonitor-importer-api/src/main/java/org/n52/kommonitor/importer/api/IndicatorsApiController.java
+++ kommonitor-importer-api/src/main/java/org/n52/kommonitor/importer/api/IndicatorsApiController.java
@@ -1,11 +1,16 @@
 package org.n52.kommonitor.importer.api;
 
+import org.n52.kommonitor.importer.api.handler.GeoresourceImportHandler;
+import org.n52.kommonitor.importer.api.handler.IndicatorImportHandler;
+import org.n52.kommonitor.importer.converter.ConverterRepository;
+import org.n52.kommonitor.importer.io.datasource.DataSourceRetrieverRepository;
 import org.n52.kommonitor.importer.models.Error;
 import org.n52.kommonitor.importer.models.ImportIndicatorPOSTInputType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -21,12 +26,22 @@ import javax.validation.Valid;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.List;
+
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-12-02T16:59:46.021+01:00")
 
 @Controller
 public class IndicatorsApiController implements IndicatorsApi {
 
-    private static final Logger log = LoggerFactory.getLogger(IndicatorsApiController.class);
+    private static final Logger LOG = LoggerFactory.getLogger(IndicatorsApiController.class);
+
+    @Autowired
+    private ConverterRepository converterRepository;
+
+    @Autowired
+    private DataSourceRetrieverRepository retrieverRepository;
+
+    @Autowired
+    private IndicatorImportHandler importHandler;
 
     private final ObjectMapper objectMapper;
 
@@ -38,18 +53,10 @@ public class IndicatorsApiController implements IndicatorsApi {
         this.request = request;
     }
 
-    public ResponseEntity<List<String>> importIndicator(@ApiParam(value = "indicator data" ,required=true )  @Valid @RequestBody ImportIndicatorPOSTInputType indicatorData) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<List<String>>(objectMapper.readValue("[ \"\", \"\" ]", List.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<List<String>>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<List<String>>(HttpStatus.NOT_IMPLEMENTED);
+    public ResponseEntity<List<String>> importIndicator(@ApiParam(value = "indicator data", required = true) @Valid @RequestBody ImportIndicatorPOSTInputType indicatorData) {
+        LOG.info("Received 'importIndicator' request for dataset name: {}", indicatorData.getDatasetName());
+        LOG.debug("'importGeoresource' POST request body: {}", indicatorData);
+        return importHandler.handleImportRequest(indicatorData, indicatorData.getDataSource(), indicatorData.getConverter());
     }
 
 }
---
+++ kommonitor-importer-api/src/main/java/org/n52/kommonitor/importer/api/handler/IndicatorImportHandler.java
@@ -0,0 +1,67 @@
+package org.n52.kommonitor.importer.api.handler;
+
+import org.n52.kommonitor.datamanagement.api.client.IndicatorsApi;
+import org.n52.kommonitor.datamanagement.api.models.IndicatorPOSTInputType;
+import org.n52.kommonitor.importer.api.encoder.IndicatorEncoder;
+import org.n52.kommonitor.importer.converter.AbstractConverter;
+import org.n52.kommonitor.importer.entities.Dataset;
+import org.n52.kommonitor.importer.entities.IndicatorValue;
+import org.n52.kommonitor.importer.exceptions.ConverterException;
+import org.n52.kommonitor.importer.exceptions.ImportParameterException;
+import org.n52.kommonitor.importer.models.ConverterDefinitionType;
+import org.n52.kommonitor.importer.models.ImportIndicatorPOSTInputType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+
+import java.util.List;
+
+/**
+ * Handles a Indicator import request by converting a {@link Dataset} into a {@link List< IndicatorValue >} and
+ * encoding it into a {@link IndicatorPOSTInputType} in order to request the KomMonitor DataManagement API
+ * for creating new Indicators.
+ *
+ * @author <a href="mailto:s.drost@52north.org">Sebastian Drost</a>
+ */
+@Component
+public class IndicatorImportHandler extends AbstractImportHandler<ImportIndicatorPOSTInputType> {
+
+    private final static Logger LOG = LoggerFactory.getLogger(IndicatorImportHandler.class);
+
+    @Autowired
+    private IndicatorEncoder encoder;
+
+    @Autowired
+    private IndicatorsApi apiClient;
+
+    @Override
+    public ResponseEntity<List<String>> importResource(ImportIndicatorPOSTInputType importResourceType,
+                                                       AbstractConverter converter,
+                                                       ConverterDefinitionType converterDefinition,
+                                                       Dataset dataset)
+            throws ConverterException, ImportParameterException, RestClientException {
+        LOG.info("Converting dataset with converter: {}", converter.getName());
+        LOG.debug("Converter definition: {}", converterDefinition);
+        List<IndicatorValue> spatialResources = converter.convertIndicators(
+                converterDefinition,
+                dataset,
+                importResourceType.getPropertyMapping());
+        IndicatorPOSTInputType indicatorPostInput = null;
+        indicatorPostInput = encoder.encode(importResourceType, spatialResources);
+
+        LOG.info("Perform 'addIndicator' request for Indicator: {}", indicatorPostInput.getDatasetName());
+        LOG.debug("'addIndicator' request POST body: {}", indicatorPostInput);
+        ResponseEntity<Void> response = apiClient.addIndicatorAsBodyWithHttpInfo(indicatorPostInput);
+        List<String> locations = response.getHeaders().get(LOCATION_HEADER_KEY);
+        LOG.info("Successfully executed 'addIndicator' request. Created Indicators: {}", locations);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(locations);
+    }
+
+}
