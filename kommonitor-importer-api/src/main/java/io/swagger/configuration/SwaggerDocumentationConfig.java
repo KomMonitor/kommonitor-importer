cd3d20d114c6a0ace4c8c1dd2189e0512f97d181
Sebastian Drost <s.drost@52north.org>
2020-06-09 08:32:01 +0200

    Reformat code

--- kommonitor-importer-api/src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
+++ kommonitor-importer-api/src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
@@ -22,22 +22,22 @@ public class SwaggerDocumentationConfig {
 
     ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-            .title("KomMonitor Data Import API")
-            .description("Import API to enable the import of spatial KomMonitor data (SpatialUnits, Georesources and Indicators) from various datasources.")
-            .license("Apache 2.0")
-            .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
-            .termsOfServiceUrl("")
-            .version("0.0.1")
-            .contact(new Contact("","", "s.drost@52north.org"))
-            .build();
+                .title("KomMonitor Data Import API")
+                .description("Import API to enable the import of spatial KomMonitor data (SpatialUnits, Georesources and Indicators) from various datasources.")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .termsOfServiceUrl("")
+                .version("0.0.1")
+                .contact(new Contact("", "", "s.drost@52north.org"))
+                .build();
     }
 
     @Bean
-    public Docket customImplementation(ServletContext servletContext, @Value("${kommonitor.swagger-ui.base-path:}") String basePath){
+    public Docket customImplementation(ServletContext servletContext, @Value("${kommonitor.swagger-ui.base-path:}") String basePath) {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                    .apis(RequestHandlerSelectors.basePackage("org.n52.kommonitor.importer.api"))
-                    .build()
+                .apis(RequestHandlerSelectors.basePackage("org.n52.kommonitor.importer.api"))
+                .build()
                 .directModelSubstitute(org.joda.time.LocalDate.class, java.sql.Date.class)
                 .directModelSubstitute(org.joda.time.DateTime.class, java.util.Date.class)
                 .pathProvider(new BasePathAwareRelativePathProvider(servletContext, basePath))
