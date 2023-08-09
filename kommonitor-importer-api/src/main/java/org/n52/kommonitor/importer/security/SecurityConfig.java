229983c4b6f9314090ae8b56aee68a72fea79111
Sebastian Drost <s.drost@52north.org>
2023-08-09 07:58:04 +0200

    Migrate to spring security for with OAuth2 support

---
+++ kommonitor-importer-api/src/main/java/org/n52/kommonitor/importer/security/SecurityConfig.java
@@ -0,0 +1,78 @@
+package org.n52.kommonitor.importer.security;
+
+import jakarta.ws.rs.HttpMethod;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig {
+
+//    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+//    String jwkSetUri;
+
+    private static String JWK_URI = "http://keycloak:8080/realms/kommonitor/protocol/openid-connect/certs";
+
+    /**
+     * Defines the session authentication strategy.
+     */
+//    @Bean
+//    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+//        return new NullAuthenticatedSessionStrategy();
+//    }
+
+    @Bean
+    @ConditionalOnProperty(
+            value="keycloak.enabled",
+            havingValue = "false",
+            matchIfMissing = false)
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement(sessions -> sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/swagger-ui/*")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/swagger-ui.html")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/v3/api-docs")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/v3/api-docs/swagger-config")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/specs/**")).authenticated()
+                        .requestMatchers(new AntPathRequestMatcher("/**")).authenticated()
+                        .anyRequest().denyAll()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+//
+    @Bean
+    @ConditionalOnProperty(
+            value="keycloak.enabled",
+            havingValue = "true",
+            matchIfMissing = false)
+    SecurityFilterChain unsecuredFilterChain(final HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
+                        .anyRequest().permitAll()
+                )
+                .build();
+    }
+
+//    @Bean
+//    JwtDecoder jwtDecoder() {
+//        return NimbusJwtDecoder.withJwkSetUri(JWK_URI).build();
+//    }
+
+}
